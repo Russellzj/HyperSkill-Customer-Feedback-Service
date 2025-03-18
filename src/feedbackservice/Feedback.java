095ff8d3172c83fcf45dd46b94e9dd32e7335f88
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2025-03-18 11:11:39 -0400

    Simplify Class

--- src/feedbackservice/Feedback.java
+++ src/feedbackservice/Feedback.java
@@ -1,6 +1,6 @@
 package feedbackservice;
 
-import lombok.Generated;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,48 +8,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Document(collection = "feedback")
 public class Feedback {
+    @Getter
     @Id
-    @Generated
     private String id;
     private int rating;
     private String feedback; //optional
     private String customer; //optional
     private String product;
     private String vendor;
-
-    public Feedback(int rating, String product, String vendor) {
-        this.rating = rating;
-        this.product = product;
-        this.vendor = vendor;
-    }
-
-    public Feedback(int rating, String product, String vendor, String feedback, String customer) {
-        this(rating, product, vendor);
-        this.feedback = feedback;
-        this.customer = customer;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public String getFeedback() {
-        return feedback;
-    }
-
-    public String getCustomer() {
-        return customer;
-    }
-
-    public String getProduct() {
-        return product;
-    }
-
-    public String getVendor() {
-        return vendor;
-    }
 }
